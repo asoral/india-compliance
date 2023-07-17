c1b8be3ac342627b0a7056d37055a8ef04e91448
Ajay <ajayjogdand374@gmail.com>
2023-07-17 16:32:34 +0530

    fix:purchase Register

--- india_compliance/gst_india/report/gst_purchase_register/gst_purchase_register.py
+++ india_compliance/gst_india/report/gst_purchase_register/gst_purchase_register.py
@@ -6,42 +6,46 @@ from erpnext.accounts.report.purchase_register.purchase_register import _execute
 import frappe
 
 def execute(filters=None):
-    additional_table_columns = get_additional_table_columns()
     columns, data = _execute(
         filters,
-        additional_table_columns
+        additional_table_columns=[
+            dict(
+                fieldtype="Data",
+                label="Supplier GSTIN",
+                fieldname="supplier_gstin",
+                width=120,
+            ),
+            dict(
+                fieldtype="Data",
+                label="Company GSTIN",
+                fieldname="company_gstin",
+                width=120,
+            ),
+            dict(
+                fieldtype="Check",
+                label="Is Reverse Charge",
+                fieldname="is_reverse_charge",
+                width=120,
+            ),
+            dict(
+                fieldtype="Data",
+                label="GST Category",
+                fieldname="gst_category",
+                width=120,
+            ),
+        ],
+        additional_query_columns=[
+            "supplier_gstin",
+            "company_gstin",
+            "is_reverse_charge",
+            "gst_category",
+        ],
     )
-   
+
     data = remove_internal_supplier_data(data)
     return columns, data
 
-def get_additional_table_columns():
-    return [
-        {
-            "fieldtype": "Data",
-            "label": _("Supplier GSTIN"),
-            "fieldname": "supplier_gstin",
-            "width": 120,
-        },
-        {
-            "fieldtype": "Data",
-            "label": _("Company GSTIN"),
-            "fieldname": "company_gstin",
-            "width": 120,
-        },
-        {
-            "fieldtype": "Check",
-            "label": _("Is Reverse Charge"),
-            "fieldname": "is_reverse_charge",
-            "width": 120,
-        },
-        {
-            "fieldtype": "Data",
-            "label": _("GST Category"),
-            "fieldname": "gst_category",
-            "width": 120,
-        },
-    ]
+
 
 
 def remove_internal_supplier_data(data):
