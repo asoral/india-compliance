1043302af39ca8b001d09c188129f5b6674a00bd
Ajay <ajayjogdand374@gmail.com>
2023-07-17 16:02:15 +0530

    fix:Internal Customer

--- india_compliance/gst_india/report/gst_purchase_register/gst_purchase_register.py
+++ india_compliance/gst_india/report/gst_purchase_register/gst_purchase_register.py
@@ -1,43 +1,58 @@
 # Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
+from frappe import _
 from erpnext.accounts.report.purchase_register.purchase_register import _execute
-
+import frappe
 
 def execute(filters=None):
-    return _execute(
+    additional_table_columns = get_additional_table_columns()
+
+    columns, data = _execute(
         filters,
-        additional_table_columns=[
-            dict(
-                fieldtype="Data",
-                label="Supplier GSTIN",
-                fieldname="supplier_gstin",
-                width=120,
-            ),
-            dict(
-                fieldtype="Data",
-                label="Company GSTIN",
-                fieldname="company_gstin",
-                width=120,
-            ),
-            dict(
-                fieldtype="Check",
-                label="Is Reverse Charge",
-                fieldname="is_reverse_charge",
-                width=120,
-            ),
-            dict(
-                fieldtype="Data",
-                label="GST Category",
-                fieldname="gst_category",
-                width=120,
-            ),
-        ],
-        additional_query_columns=[
-            "supplier_gstin",
-            "company_gstin",
-            "is_reverse_charge",
-            "gst_category",
-        ],
+        additional_table_columns
     )
+   
+    data = remove_internal_supplier_data(data)
+    return columns, data
+
+def get_additional_table_columns():
+    return [
+        {
+            "fieldtype": "Data",
+            "label": _("Supplier GSTIN"),
+            "fieldname": "supplier_gstin",
+            "width": 120,
+        },
+        {
+            "fieldtype": "Data",
+            "label": _("Company GSTIN"),
+            "fieldname": "company_gstin",
+            "width": 120,
+        },
+        {
+            "fieldtype": "Check",
+            "label": _("Is Reverse Charge"),
+            "fieldname": "is_reverse_charge",
+            "width": 120,
+        },
+        {
+            "fieldtype": "Data",
+            "label": _("GST Category"),
+            "fieldname": "gst_category",
+            "width": 120,
+        },
+    ]
+
+
+def remove_internal_supplier_data(data):
+    new_data = [
+        row
+        for row in data
+        
+        if not frappe.get_cached_value(
+            "Supplier", row[2], "is_internal_supplier"
+        )
+    ]
+
+    return new_data
